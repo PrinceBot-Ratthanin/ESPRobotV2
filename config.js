6d0dd939ea0512c30093deda2f311d918d79102f
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2024-11-22 23:08:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'ESPRobotV2',
+    platform : 'arduino-esp32',
+    title : 'ESPRobotV2',
+    description : 'บอร์ดสำหรับการเรียนรู้การควบคุมหุ่นยนต์เบื้องต้น',
+    author : 'PrinceBot',
+    website : 'http://www.shop.princebot.co.th/',
+    email : 'print081@gmail.com',
+    git : 'https://github.com/PrinceBot-Ratthanin/ESPRobotV2-KB-IDE/',    
+    image : '/static/ESPRobotV2.png',
+    version : '1.0'
+};
